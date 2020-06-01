3b1709196acf24231b778f610a6df8c1481a8c08
Rafael lima <rafaellrf09@gmail.com>
2020-06-01 12:28:07 -0300

    new class teste

--- src/teste/teste.controller.ts
+++ src/teste/teste.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { Teste } from './teste.entity'
 @Controller('teste')
 export class TesteController {
     
     @Get()
-    getTeste(): JSON[] {
-        const json = '{"result":true, "count":42}';
-        const json2 = '{"name": "Jonas", "idade":32 }';
-        const obj1 = JSON.parse(json);
-        const obj2 = JSON.parse(json2);
-        return [obj1,obj2];
+    getTeste(): Teste[] {
+        const testes: Teste[] = [];
+        testes.push(new Teste("Rola", 50))
+        testes.push(new Teste("Rola2", 30))
+        testes.push(new Teste("Rola3", 10))
+        
+        return testes;
     }
 
 
---
+++ src/teste/teste.entity.ts
@@ -0,0 +1,3 @@
+export class Teste {
+    constructor(public name: string, public age: number){}    
+}
